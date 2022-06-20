e5ae0119f6a33db4f12d3a524a5365c32a796af9
FeaR-EdgE <107889128+FeaR-EdgE@users.noreply.github.com>
2022-06-20 15:08:52 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`heart key`, function (sprite, location) {
+    game.over(true, effects.hearts)
+})
 let Bon = sprites.create(assets.image`Bon`, SpriteKind.Player)
 Bon.setStayInScreen(true)
 scene.cameraFollowSprite(Bon)
 tiles.setCurrentTilemap(tilemap`level102`)
 tiles.placeOnTile(Bon, tiles.getTileLocation(0, 8))
+tiles.setTileAt(tiles.getTileLocation(15, 8), assets.tile`heart key`)
 forever(function () {
     Bon.setVelocity(0, 50)
     if (controller.left.isPressed()) {
-        Bon.x += -3
+        Bon.x += -1
     }
     if (controller.right.isPressed()) {
-        Bon.x += 3
+        Bon.x += 1
     }
 })
 forever(function () {
