6529cebd6197cd4e09d13b6f574ec8971d681cfb
FeaR-EdgE <107889128+FeaR-EdgE@users.noreply.github.com>
2022-06-21 13:58:18 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,33 @@
 namespace SpriteKind {
     export const Player2 = SpriteKind.create()
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`heart key`, function (sprite, location) {
-    tiles.placeOnTile(Bon, tiles.getTileLocation(0, 13))
-    tiles.setTileAt(tiles.getTileLocation(15, 14), img`myTile8`)
-    Bon.sayText(":3", 200, false)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
+    tiles.placeOnTile(Don, tiles.getTileLocation(0, 8))
 })
-scene.onOverlapTile(SpriteKind.Player, img`myTile7`, function (sprite, location) {
-    tiles.placeOnTile(Bon, tiles.getTileLocation(0, 8))
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, location) {
+    tiles.placeOnTile(Don, tiles.getTileLocation(0, 13))
+    tiles.setTileAt(tiles.getTileLocation(15, 14), assets.tile`myTile11`)
+    Don.sayText("Continue the Search for Keys", 200, false)
 })
-scene.onOverlapTile(SpriteKind.Player, img`myTile8`, function (sprite, location) {
-    game.over(true, effects.hearts)
-    pause(5000)
-    game.reset()
-})
-let Bon: Sprite = null
-Bon = sprites.create(assets.image`Bon`, SpriteKind.Player)
-Bon.setStayInScreen(true)
-scene.cameraFollowSprite(Bon)
+let Don: Sprite = null
+Don = sprites.create(assets.image`Don`, SpriteKind.Player)
+Don.setStayInScreen(true)
+scene.cameraFollowSprite(Don)
 tiles.setCurrentTilemap(tilemap`level102`)
-tiles.placeOnTile(Bon, tiles.getTileLocation(0, 8))
-tiles.setTileAt(tiles.getTileLocation(15, 8), assets.tile`heart key`)
-scene.setBackgroundColor(9)
+tiles.placeOnTile(Don, tiles.getTileLocation(0, 8))
+tiles.setTileAt(tiles.getTileLocation(15, 8), assets.tile`myTile10`)
 forever(function () {
-    Bon.setVelocity(0, 50)
+    Don.setVelocity(0, 50)
     if (controller.left.isPressed()) {
-        Bon.x += -1
+        Don.x += -1
     }
     if (controller.right.isPressed()) {
-        Bon.x += 1
+        Don.x += 1
     }
 })
 forever(function () {
     if (controller.up.isPressed()) {
-        Bon.y += -40
+        Don.y += -40
         pause(1000)
     }
 })
