bc8798e7ba08e8bf286e2495f8a8c659ccde4e28
FeaR-EdgE <107889128+FeaR-EdgE@users.noreply.github.com>
2022-06-21 16:58:44 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,23 @@
 namespace SpriteKind {
     export const Player2 = SpriteKind.create()
-}
+} 
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
     tiles.placeOnTile(Don, tiles.getTileLocation(0, 8)) , info.changeScoreBy (-10)
-    Don.sayText("Haha You Suck at This Game", 1000, false)
+Don.sayText("Don't Touch That", 3000, false)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, location) {
     tiles.placeOnTile(Don, tiles.getTileLocation(0, 13))
     tiles.setTileAt(tiles.getTileLocation(15, 14), assets.tile`myTile11`)
     Don.sayText("Continue the Search for Keys", 2000, false) , info.changeScoreBy (100)
 })
+let Don: Sprite = null
+let ScoreCount = 0
 let transitionToNextLevelThree = function (sprite:Sprite, location:tiles.Location) {
     tiles.placeOnTile(Don, tiles.getTileLocation(32, 7))
     tiles.setTileAt(tiles.getTileLocation(57, 7), assets.tile`myTile12`)
     Don.sayText("Avoid the White Explosives", 2000, false), info.changeScoreBy(100)
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, transitionToNextLevelThree )
-
-let ScoreCount = 0
-let Don: Sprite = null
 Don = sprites.create(assets.image`Don`, SpriteKind.Player)
 Don.setStayInScreen(true)
 scene.cameraFollowSprite(Don)
@@ -30,6 +29,15 @@ let DonAbility = textsprite.create("Don has the Ability to Teleport Jump")
 tiles.placeOnTile(IntroText, tiles.getTileLocation(8, 5))
 tiles.placeOnTile(DonAbility, tiles.getTileLocation(8, 4))
 info.setScore(0)
+forever(function () {
+    Don.setVelocity(0, 50)
+    if (controller.left.isPressed()) {
+        Don.x += -1
+    }
+    if (controller.right.isPressed()) {
+        Don.x += 1
+    }
+})
 forever(function () {
     if (controller.up.isPressed()) {
         Don.y += -40
@@ -43,14 +51,5 @@ forever(function () {
             IntroText.destroy()
             DonAbility.destroy()
         }
-    }
-})
-forever(function () {
-    Don.setVelocity(0, 50)
-    if (controller.left.isPressed()) {
-        Don.x += -1
-    }
-    if (controller.right.isPressed()) {
-        Don.x += 1
     }
 })
